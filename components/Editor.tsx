872d011dd89a5e911720f4edc9670b12afa41512
lichin-lin <vic20087cjimlin@gmail.com>
2023-07-09 01:00:48 +0800

    feat: cmdk

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -9,7 +9,10 @@ import {
   defaultTools,
   useEditor,
 } from "@tldraw/tldraw";
+import "react-cmdk/dist/cmdk.css";
+import CommandPalette, { filterItems, getItemIndex } from "react-cmdk";
 import { nanoid } from "nanoid";
+import { useState, useEffect } from "react";
 
 export default function CustomUiExample() {
   return (
@@ -27,7 +30,11 @@ export default function CustomUiExample() {
 }
 
 const CustomUi = () => {
+  const [page, setPage] = useState<"root" | "projects">("root");
+  const [open, setOpen] = useState<boolean>(false);
+  const [search, setSearch] = useState("");
   const editor = useEditor();
+
   const handleSummarizer = async () => {
     // set loading status = true
 
@@ -86,17 +93,97 @@ const CustomUi = () => {
       editor.selectNone();
     }
   };
+  useEffect(() => {
+    function handleKeyDown(e: KeyboardEvent) {
+      if (e.metaKey && e.key === "k") {
+        e.preventDefault();
+        e.stopPropagation();
+
+        setOpen((currentValue) => {
+          return !currentValue;
+        });
+      }
+    }
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+  // https://heroicons.com/
+  const filteredItems = filterItems(
+    [
+      {
+        heading: "AI tryout",
+        id: "ai",
+        items: [
+          {
+            id: "summarizer",
+            children: "Summarizer",
+            icon: "SparklesIcon",
+            closeOnSelect: true,
+            onClick: () => {
+              handleSummarizer();
+            },
+          },
+          {
+            id: "text2img",
+            children: "Text to Image",
+            icon: "SparklesIcon",
+            closeOnSelect: true,
+            onClick: () => {
+              console.log("ai!");
+            },
+          },
+        ],
+      },
+      {
+        heading: "Editing experience",
+        id: "editor",
+        items: [
+          {
+            id: "camera",
+            children: "Cursor Camera",
+            icon: "CameraIcon",
+            closeOnSelect: true,
+            onClick: () => {
+              console.log("camera!");
+            },
+          },
+        ],
+      },
+    ],
+    search
+  );
+
   return (
-    <div className="custom-layout">
-      <div className="custom-toolbar">
-        <button
-          className="custom-button"
-          data-isactive={editor.currentToolId === "select"}
-          onClick={() => handleSummarizer()}
-        >
-          AI Summarizer
-        </button>
-      </div>
-    </div>
+    <>
+      <CommandPalette
+        onChangeSearch={setSearch}
+        onChangeOpen={setOpen}
+        search={search}
+        isOpen={open}
+        page={page}
+      >
+        <CommandPalette.Page id="root">
+          {filteredItems.length ? (
+            filteredItems.map((list) => (
+              <CommandPalette.List key={list.id} heading={list.heading}>
+                {list.items.map(({ id, ...rest }) => (
+                  <CommandPalette.ListItem
+                    key={id}
+                    index={getItemIndex(filteredItems, id)}
+                    {...rest}
+                  />
+                ))}
+              </CommandPalette.List>
+            ))
+          ) : (
+            <CommandPalette.FreeSearchAction />
+          )}
+        </CommandPalette.Page>
+      </CommandPalette>
+    </>
   );
 };
