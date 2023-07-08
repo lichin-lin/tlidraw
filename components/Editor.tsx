0216e027ca4a8b771e9fdea3614111954d9187d2
lichin-lin <vic20087cjimlin@gmail.com>
2023-07-08 21:54:01 +0800

    feat: add server endpoint

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,13 +1,15 @@
 import {
   Canvas,
   ContextMenu,
+  TLNoteShape,
+  TLShapeId,
   TldrawEditor,
   TldrawUi,
   defaultShapes,
   defaultTools,
   useEditor,
 } from "@tldraw/tldraw";
-import { useEffect } from "react";
+import { nanoid } from "nanoid";
 
 export default function CustomUiExample() {
   return (
@@ -16,7 +18,7 @@ export default function CustomUiExample() {
         <TldrawUi>
           <ContextMenu>
             <Canvas />
-			{/* <CustomUi /> */}
+            <CustomUi />
           </ContextMenu>
         </TldrawUi>
       </TldrawEditor>
@@ -25,49 +27,77 @@ export default function CustomUiExample() {
 }
 
 const CustomUi = () => {
-	const editor = useEditor()
+  const editor = useEditor();
+  const handleSummarizer = async () => {
+    // set loading status = true
 
-	useEffect(() => {
-		const handleKeyUp = (e: KeyboardEvent) => {
-			switch (e.key) {
-				case 'Delete':
-				case 'Backspace': {
-					editor.deleteShapes()
-				}
-			}
-		}
+    // create prompt
+    const prompt = (
+      editor.selectedShapes.filter((s) => s.type === "note") as TLNoteShape[]
+    )
+      .map((s) => s.props?.text)
+      .join("\n ---- \n");
+    const bound = editor.selectionBounds;
+    const newPos = bound
+      ? [bound.x + bound.w + 200, bound.y + bound.h / 2]
+      : [200, 200];
+    console.log(bound, newPos);
 
-		window.addEventListener('keyup', handleKeyUp)
-		return () => {
-			window.removeEventListener('keyup', handleKeyUp)
-		}
-	})
+    // generate new sticky + content
+    const id = `shape:${nanoid()}` as TLShapeId;
+    editor.createShapes([
+      {
+        id: id,
+        type: "note",
+        x: newPos[0],
+        y: newPos[1],
+        props: {
+          text: "🤖 AI is reading the notes...",
+          size: "s",
+          font: "mono",
+          color: "grey",
+          align: "middle",
+        },
+      },
+    ]);
+    // fly to
+    editor.select(id);
+    editor.zoomToSelection({ duration: 1000 });
 
-	return (
-		<div className="custom-layout">
-			<div className="custom-toolbar">
-				<button
-					className="custom-button"
-					data-isactive={editor.currentToolId === 'select'}
-					onClick={() => editor.setSelectedTool('select')}
-				>
-					Select
-				</button>
-				<button
-					className="custom-button"
-					data-isactive={editor.currentToolId === 'draw'}
-					onClick={() => editor.setSelectedTool('draw')}
-				>
-					Pencil
-				</button>
-				<button
-					className="custom-button"
-					data-isactive={editor.currentToolId === 'eraser'}
-					onClick={() => editor.setSelectedTool('eraser')}
-				>
-					Eraser
-				</button>
-			</div>
-		</div>
-	)
-}
+    // AI
+    const response = await fetch("/api/summarizer", {
+      method: "POST",
+      body: JSON.stringify({
+        prompt,
+      }),
+    });
+    const res = await response.json();
+    if (res.data.choices) {
+      editor.updateShapes([
+        {
+          id: id,
+          type: "note",
+          props: {
+            text: `Summarized by AI 🌿\n${res?.data?.choices[0]?.message?.content}`,
+            align: "start",
+          },
+        },
+      ]);
+      editor.zoomToSelection({ duration: 500 });
+      editor.selectNone();
+    }
+  };
+  return (
+    <div className="custom-layout">
+      <div className="custom-toolbar">
+        <button
+          className="custom-button"
+          data-isactive={editor.currentToolId === "select"}
+          onClick={() => handleSummarizer()}
+        >
+          AI Summarizer
+        </button>
+      </div>
+    </div>
+  );
+};
---
+++ pages/api/summarizer.ts
@@ -0,0 +1,42 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+//  "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";
+const GPT_KEY = "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";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  let body = JSON.parse(req.body);
+  const { prompt } = body;
+
+  try {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${GPT_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content: `Please summarize the following paragraph, input sections are seperated with "----", please generate output as several bullet point (at most 5, I want each bullet point start with a emoji, please give suitable emoji depends on the context, if you really can't find any suitable emoji, just put: "📍") in markdown format: ${prompt}.`,
+          },
+        ],
+        max_tokens: 2000,
+      }),
+    });
+    const data = await response.json();
+    res.status(200).json({
+      message: "success",
+      data,
+    });
+  } catch (err) {
+    console.log("error: ", err);
+    res.status(500).json({
+      message: `failed: ${err}`,
+    });
+  }
+}
