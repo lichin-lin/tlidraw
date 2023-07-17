a6f3d5a527f71c2694105f3fad85521642db5d7e
lichin-lin <vic20087cjimlin@gmail.com>
2023-07-18 00:15:24 +0800

    feat: doodle crayon efffect

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -97,6 +97,7 @@ const CustomUi = () => {
   const [page, setPage] = useState<"root" | "projects">("root");
   const [open, setOpen] = useState<boolean>(false);
   const [showJoystick, setShowJoystick] = useState<boolean>(true);
+  const [crayonEffect, setCrayonEffect] = useState<boolean>(false);
   const [joystickStatus, setJoystickStatus] = useState<JoystickStatus>("stop");
   const [shapeData, setShapeData] = useState<any>([]);
   const [linkedListMode, setLinkedListMode] = useState<boolean>(false);
@@ -494,6 +495,15 @@ const CustomUi = () => {
               // moveDegree(20);
             },
           },
+          {
+            id: "crayon",
+            children: "Crayon effect (for Doodle)",
+            icon: "PencilIcon",
+            closeOnSelect: true,
+            onClick: () => {
+              setCrayonEffect(!crayonEffect);
+            },
+          },
         ],
       },
     ],
@@ -593,6 +603,53 @@ const CustomUi = () => {
   }, [editor, linkedListMode]);
   return (
     <>
+      {/* Sketch effect */}
+      {crayonEffect && (
+        <svg style={{ width: 0, height: 0, position: 'absolute' }}>
+          <defs>
+            <filter
+              x="0%"
+              y="0%"
+              width="100%"
+              height="100%"
+              filterUnits="objectBoundingBox"
+              id="pencilTexture"
+            >
+              <feTurbulence
+                type="fractalNoise"
+                baseFrequency="0.5"
+                numOctaves="5"
+                stitchTiles="stitch"
+                result="f1"
+              ></feTurbulence>
+              <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0, 0 0 0 0 0, 0 0 0 0 0, 0 0 0 -1.5 1.5"
+                result="f2"
+              ></feColorMatrix>
+              <feComposite
+                operator="in"
+                in2="f2b"
+                in="SourceGraphic"
+                result="f3"
+              ></feComposite>
+              <feTurbulence
+                type="fractalNoise"
+                baseFrequency="1.2"
+                numOctaves="3"
+                result="noise"
+              ></feTurbulence>
+              <feDisplacementMap
+                xChannelSelector="R"
+                yChannelSelector="G"
+                scale="2.5"
+                in="f3"
+                result="f4"
+              ></feDisplacementMap>
+            </filter>
+          </defs>
+        </svg>
+      )}
       <CommandPalette
         onChangeSearch={setSearch}
         onChangeOpen={setOpen}
